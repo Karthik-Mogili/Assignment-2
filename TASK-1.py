0138571ef7090bf3c2d7ed99920b10dab20d4905
Karthik-Mogili <mog.karthik0001@gmail.com>
2025-06-15 19:59:33 +0530

    Add files via upload
    Added Task 1 and Task 2 Python Scripts

---
+++ TASK-1.py
@@ -0,0 +1,8 @@
+# Task 1: Check if a Number is Even or Odd
+
+num = int(input("Enter an number: "))
+
+if num % 2 == 0:
+    print(f"{num} is an even number.")
+else:
+    print(f"{num} is an Odd number.")
